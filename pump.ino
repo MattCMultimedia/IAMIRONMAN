dd94ec692d39b1b3f2a4a30d9f8ecb0df2d8f2b9
Matt Condon <MattCMultimedia@gmail.com>
2014-01-13 14:19:13 -0600

    init commit with scaffolding

---
+++ pump.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+#define SETPOINT 0.12
+#define STD_ERR 0.01
+#define LHL SETPOINT-STD_ERR
+#define RHL SETPOINT+STD_ERR
+
+//
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 10);
+
+int salinity_trigger = 11;
+int salinity_pin = A5;
+
+
+void setup()
+{
+    lcd.begin(20, 4);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // for the main loop
+    // we want to check the salinity
+    // if salinity is below LHL OR above RHL, correct it
+    int salinity = salinity();
+
+}
+
+
+
+
+int salinity()
+{
+    digitalWrite(salinity_trigger, HIGH);
+    delay(100);
+    int x = analogRead(salinity_pin);
+    digitalWrite(salinity_trigger, LOW);
+    return x;
+}
