4527b2a7a7848ef9ea6d0850063eddaf6c830259
Matt Condon <MattCMultimedia@gmail.com>
2014-01-13 19:23:11 -0600

    added comments explaining variables

--- pump.ino
+++ pump.ino
@@ -1,16 +1,31 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 
-#define SETPOINT 0.12
-#define STD_ERR 0.01
-#define LHL SETPOINT-STD_ERR
-#define RHL SETPOINT+STD_ERR
+#define SETPOINT (0.12)
+#define STD_ERR (46/1024)
 
-//
+// LHL and RHL calculated as linear delta offset compared to setpoint
+#define LHL (SETPOINT-STD_ERR)
+#define RHL (SETPOINT+STD_ERR)
+
+// Gain
+#define GAIN 0.8
+
+// %wt of the salt in the salty container
+#define PWT_SALT = 0.25
+
+// lcd
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 10);
 
+// pin that powers salinity sensor
 int salinity_trigger = 11;
-int salinity_pin = A5;
+// analog pin that measures voltage drop over 10kO resistor
+int salinity_pin = A0;
+
+// soleniod salt
+int ss = 9;
+// solenoid di
+int sdi = 8;
 
 
 void setup()
@@ -24,20 +39,22 @@ void loop()
     // for the main loop
     // we want to check the salinity
     // if salinity is below LHL OR above RHL, correct it
-    int salinity = salinity();
 
-    if (salinity < LHL) {
+    // int salinity = getSalinity();
+    // Serial.println(salinity);
 
-    } else if (salinity > RHL) {
+    // if (salinity < LHL) {
 
-    }
+    // } else if (salinity > RHL) {
+
+    // }
 
 }
 
 
 
 
-int salinity()
+int getSalinity()
 {
     digitalWrite(salinity_trigger, HIGH);
     delay(100);
@@ -48,10 +65,24 @@ int salinity()
 
 void openDIForSeconds(float seconds)
 {
-
+    digitalWrite(sdi, HIGH);
+    delay(2000);
+    digitalWrite(sdi, LOW);
 }
 
 void openSaltForSeconds(float seconds)
 {
+    digitalWrite(ss, HIGH);
+    delay(2000);
+    digitalWrite(ss, LOW);
+}
 
+float salinityToAnalog(float s)
+{
+    return 22.9465601051 * log(s) + 197.0368452225;
+}
+
+float analogToSalinity(float a)
+{
+    return 0.7419333138* log(a) - 3.6131359758
 }
